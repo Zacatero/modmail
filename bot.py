6322a5f4ca22de176171f14cb7c02163aef14946
Zacatero <34138928+Zacatero@users.noreply.github.com>
2018-01-11 19:01:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -193,7 +193,7 @@ class Modmail(commands.Bot):
                     if 'User ID:' in str(chan.topic):
                         user_id = int(chan.topic.split(': ')[1])
                         user = self.get_user(user_id)
-                        await user.send(f'**{ctx.author}** has closed this modmail session.')
+                        await user.send(f'**{ctx.author}** has shutdown this program. If you believe this is an error, please let a Moderator know.')
                     await chan.delete()
         await categ.delete()
         await ctx.send('Disabled modmail.')
